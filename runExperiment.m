b5bbee0e43541f1f0d7163ca28712238008bf2f3
jonathan-sahar-tau <sahar@tau.ac.il>
2022-03-26 09:49:20 +0300

    Working with 4 image blocks - before making two block types

--- runExperiment.m
+++ runExperiment.m
@@ -15,23 +15,23 @@ runFirstPart = 0;
 runSecondPart = 1;
 
 numBlocks = 4;
-numClassificationBlocks = 4
+num_classificationBlocks = 4;
 numCatchTrialBlocks = 2;
-numImagesPerBlock = 8;
-numImagesPerClassificationBlock = 6;
-catchTrialBlockLocations = [2, numBlocks - 1]
-catchTrialBlockLocationsClassification = [2, numClassificationBlocks - 1]
+numImagesPerBlock = 4;
+numImagesPer_classificationBlock = 6;
+catchTrialBlockLocations = [2, numBlocks - 1];
+catchTrialBlockLocations_classification = [2, num_classificationBlocks - 1];
 % randsample(2:numBlocks-1,numCatchTrialBlocks)
 
 fixationCrossSize = 20; %size of fixation cross in pixels
 fixationCoords = [[-fixationCrossSize fixationCrossSize 0 0]; [0 0 -fixationCrossSize fixationCrossSize]];%setting fixation point coordinations
 lineWidthFixation = 4; %line width of fixaton cross in pixels
 stimSize=[0 0 100 100]; %Set Stimulus Dimantions [top-left-x, top-left-y, bottom-right-x, bottom-right-y].
-dispDuration=1;
+dispDuration= 0.75;
  % duration of each image display
-waitDuration=2;
+blockWaitDuration=2;
  % duration to wait betewen blocks
-betweenShapesDuration = 0.5; % duration to wait between same-shape displays in a clssification block
+betweenShapesDuration = 0.25; % duration to wait between same-shape displays in a clssification block
 
 
 %% initiate psychtoolbox
@@ -41,10 +41,35 @@ end
 
 %% load images of shapes
 % images = loadImages('/mnt/g/My Drive/Msc neuroscience/lab - mukamel/code/stimuli-presentation-for-fMRI/shape-images/')
-images = loadImages('.\shape-images\')
+images = loadImages('.\shape-images\centered\')
 
  
- if runFirstPart
+
+
+%% which subject are we running? keep its stats
+load('./subjectData/latestSubjectId.mat')
+subjectId = subjectId + 1;
+save ./subjectData/latestSubjectId.mat subjectId;
+
+logFile = "log_" + subjectId + ".mat"
+
+startTimes = [];
+startTimes_classification = [];
+endTimes = [];
+endTimes_classification = [];
+
+imageIndex_classification = [];
+
+catchTrialIndex = zeros(1, numBlocks);
+catchTrialIndex(1,catchTrialBlockLocations) = 1;
+
+catchTrialIndex_classification = zeros(1, num_classificationBlocks);
+catchTrialIndex_classification(1,catchTrialBlockLocations_classification) = 1;
+
+% start the clock 
+startTic = tic;
+ 
+if runFirstPart
 
 
      %% run the experiment - part 1
@@ -53,22 +78,31 @@ images = loadImages('.\shape-images\')
          if ismember(i, catchTrialBlockLocations) % if it's a catch trial - double one of the images
              imageIndices(mod(i, numImagesPerBlock)+1) = imageIndices(mod(i+1, numImagesPerBlock)+1)
          end
+         %log the start of this block
+         startTimes(1,end+1)= toc(startTic);
 
          for imgIdx = imageIndices
              % iterate over block images
              img = images{imgIdx};
              Screen('PutImage', window, img); % put image on screen
+             Screen('DrawLines', window, fixationCoords, lineWidthFixation, black, [xCenter yCenter], 2);
+
              Screen('Flip', window);
+
              WaitSecs(dispDuration);
              
              Screen(window,'FillRect') % clear the screen between shapes
+             Screen('DrawLines', window, fixationCoords, lineWidthFixation, black, [xCenter yCenter], 2);
+
              Screen('Flip', window);
              WaitSecs(betweenShapesDuration);
          end
-
+         % log the end of this block:
+         endTimes(1,end+1)= toc(startTic);
+         
          Screen('DrawLines', window, fixationCoords, lineWidthFixation, black, [xCenter yCenter], 2);
          Screen('Flip', window);
-         WaitSecs(waitDuration);
+         WaitSecs(blockWaitDuration);
          
      end
  end
@@ -94,21 +128,31 @@ images = loadImages('.\shape-images\')
      %% run the experiment - part 2
      
      indexPairs = [mostConfusedPair; leastConfusedPair];
-     % run two cycles of blocks - with the most- and leas-confused pairs
-     for p = size(indexPairs,1) 
+     % run two cycles of blocks - with the most- and least-confused pairs
+     for p = 1:size(indexPairs,1) 
          indexPair = indexPairs(p,1:size(indexPairs,1));
-         blockIndices = [indexPair(1) * ones(1, numClassificationBlocks/2), indexPair(2) * ones(1,numClassificationBlocks/2)]; % every block is either 1 or 2, which is the index of the (single) shape that appears throughout the block.
+
+          % every block is either 1 or 2, which is the index of the (single) shape that appears throughout the block.
+         blockIndices = [indexPair(1) * ones(1, num_classificationBlocks/2), indexPair(2) * ones(1,num_classificationBlocks/2)]
+
          blockIndices = blockIndices(randperm(length(blockIndices))); % shuffle the blocks
-         for i=1:numClassificationBlocks
+         num_classificationBlocks
+         for i=1:num_classificationBlocks
+             
              % create the indices to use for displaying the shape(s) in this block
-             imageIndices = repmat(blockIndices(i), 1, numImagesPerClassificationBlock);
-             if ismember(i, catchTrialBlockLocationsClassification)
+             imageIndices = repmat(blockIndices(i), 1, numImagesPer_classificationBlock);
+             if ismember(i, catchTrialBlockLocations_classification)
                  % if it's a catch trial - change one of the images to the the other shape
                  % since the sum of the indices of the shapes is constant, we get the other  shape by subtracting from the total
                  otherShapeIndex = sum(indexPair) - blockIndices(i);
-                 imageIndices(mod(i, numImagesPerClassificationBlock) + 1) = otherShapeIndex;
+                 imageIndices(mod(i, numImagesPer_classificationBlock) + 1) = otherShapeIndex;
              end
-             
+
+             imageIndex_classification = [imageIndex_classification imageIndices
+                   ];
+
+             % log the start of this block
+             startTimes_classification(1, end+1) = toc(startTic);
              for imgIdx= imageIndices % iterate over block images
                  img = images{imgIdx};
                  Screen('PutImage', window, img); % put image on screen
@@ -117,14 +161,14 @@ images = loadImages('.\shape-images\')
                  
                  Screen(window,'FillRect') % clear the screen between shapes
                  Screen('Flip', window);
-                 WaitSecs(betweenShapesDuration);
-                 
-                 
+                 WaitSecs(betweenShapesDuration);   
              end
              
+             % log the end of this block
+             endTimes_classification(1, end+1) = toc(startTic);
              Screen('DrawLines', window, fixationCoords, lineWidthFixation, black, [xCenter yCenter], 2);
              Screen('Flip', window);
-             WaitSecs(waitDuration);
+             WaitSecs(blockWaitDuration);
              
          end
      end
@@ -133,7 +177,7 @@ images = loadImages('.\shape-images\')
  Screen('CloseAll')
  
  %ShwCursor
- 
+save (logFile, "startTimes", "endTimes", "startTimes_classification", "endTimes_classification", "imageIndex_classification", "catchTrialIndex", "catchTrialIndex_classification")
  
  
  function images = loadImages(dir)
@@ -142,14 +186,11 @@ images = loadImages('.\shape-images\')
      uiwait(warndlg(errorMessage));
      return;
  end
- imageDS = imageDatastore(dir,"FileExtensions",[".png"]);
+ imageDS = imageDatastore(dir,"FileExtensions",".png");
  images = readall(imageDS);
  end
  
  
- 
- %Add_Psych;
- %HideCursor;
  function [window, xCenter, yCenter, black, white] = initScreen()
  Screen('Preference', 'VisualDebuglevel', 3); %No PTB intro screen
  Screen('Preference', 'SkipSyncTests', 1); %change to 0 in real experiment
